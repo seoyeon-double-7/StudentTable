7f340b367ca46ca98ea857ae8e49fa7dae9bc6ef
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-08-10 08:48:50 +0900

    데이터 그리드뷰 & 버튼 5개 추가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,99 @@ namespace StudentTable
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(753, 288);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "전공";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "이름";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "학번";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(504, 44);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 68);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "수학점수";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(649, 44);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 68);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "영어점수";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "3205 배서연";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
