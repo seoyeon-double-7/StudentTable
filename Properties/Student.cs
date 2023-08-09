5f29290350f31582a93b5d6e22af4b64c0d0867b
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-08-10 08:49:46 +0900

    Student 파일 만들고, 속성 정의

---
+++ Properties/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTable
+{
+    internal class Student
+    {
+        public string major;
+        public string name;
+        public int student_number;
+        public int math_score;
+        public int english_score;
+    }
+}
